5b9251f28f74150c2245dcbb7144d3fcd4d2a809
Ripa12 <richard93.petersson@gmail.com>
2017-05-17 17:11:41 +0200

    Test case for user story Strike; Added test for checking if a strike is followed by a pointless throw in a frame

--- src/TestFrame.java
+++ src/TestFrame.java
@@ -7,13 +7,15 @@ import org.junit.Assert;
 public class TestFrame {
 	protected Frame frameObject;
 	protected Frame frameZeroScore;
-	protected Frame frameFullScore;
+	protected Frame frameTenScore;
+	protected Frame frameStrike;
 	
 	@Before 
 	public void setup() {
 		frameObject = new Frame();
 		frameZeroScore = new Frame(0, 0);
-		frameFullScore  = new Frame(10, 10);
+		frameTenScore = new Frame(5, 5);
+		frameStrike = new Frame(10, 0);
 	}
 	
 	/* 
@@ -44,17 +46,26 @@ public class TestFrame {
 	 * where both throws miss every pin
      */
 	@Test
-	public void testComputeScoreZeroPoints() {
+	public void testComputeScoreZeroPins() {
 		assertTrue("Incorrect score computed: " + frameZeroScore.computeScore(), frameZeroScore.computeScore() == (0));
 	}
 	
 	/* 
 	 * Test the computed score for one single frame
-	 * where both throws hit every pin
+	 * where both throws hit half of the pins
      */
 	@Test
-	public void testComputeScoreFullPoints() {
-		assertTrue("Incorrect score computed: " + frameFullScore.computeScore(), frameFullScore.computeScore() == (20));
+	public void testComputeScoreHalfOfPins() {
+		assertTrue("Incorrect score computed: " + frameTenScore.computeScore(), frameTenScore.computeScore() == (10));
+	}
+	
+	/* 
+	 * Test if second throw of a frame is equal to zero after a strike
+     */
+	@Test
+	public void testStrike() {
+		assertTrue("First throw is not a strike: " + frameStrike.getThrows()[0], frameStrike.getThrows()[0] == (10));
+		assertTrue("Second throw is not pointless: " + frameStrike.getThrows()[1], frameStrike.getThrows()[1] == (0));
 	}
 	
 }
